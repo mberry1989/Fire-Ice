3e9ae31ccb60ec5023f06b4c9a1e3960b5eef6f3
Michael Berry <mberry1989@gmail.com>
2024-09-13 20:14:35 -0400

    Adds vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider attribute="class">
           <div>{children}</div>
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
